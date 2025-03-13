bd9399c3f97fe52bd3343c92f50deda7139a49e6
SomoodMsleh <somoodmedwan@gmail.com>
2025-03-13 13:45:16 +0200

    anther way to add data in register router

--- src/Modules/Auth/auth.controller.js
+++ src/Modules/Auth/auth.controller.js
@@ -2,9 +2,11 @@ import userModel from "../../../DB/Models/user.model.js";
 import bcrypt from "bcryptjs";
 
 export const registerUser = async (req,res)=>{
-    const {userName,email,password} = req.body;
-    const hashPassword = bcrypt.hashSync(password,8)
-    const user = await userModel.create({userName,email,password:hashPassword});
+    const {userName,email,password,age} = req.body;
+    const hashPassword = bcrypt.hashSync(password,8);    
+    //const user = await userModel.create({userName,email,password:hashPassword});
+    const user = new userModel({userName,email,password:hashPassword,age});
+    await user.save();
     return res.status(201).json({message:"successfully",user});
 } 
 
