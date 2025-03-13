b1ee9051cb0b2e32a03c0d68b70f5c267cff4dd5
SomoodMsleh <somoodmedwan@gmail.com>
2025-03-13 12:36:56 +0200

    user Model

--- DB/Models/user.model.js
+++ DB/Models/user.model.js
@@ -1,19 +1,27 @@
 import { model, Schema, Types } from "mongoose";
 const userSchema = new Schema({
     userName:{
-        
+        type:String,
+        required:true
     },
     email:{
-
+        type:String,
+        required:true
     },
     password:{
-
+        type:String,
+        required:true
     },
     confirmEmail:{
-
+        type:Boolean,
+        default:false
     },
     gender:{
-
+        type:String,
+        enum:['Male','Female']
+    },
+    age:{
+        type:Number
     }
 });
 
