db4053639314ccf9e9206ab744748f077a773c87
SomoodMsleh <somoodmedwan@gmail.com>
2025-04-02 01:42:17 +0200

    upload database to Atlas

--- DB/connection.js
+++ DB/connection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 async function connectDB() {
     try{
-        await mongoose.connect('mongodb://localhost:27017/test');
+        await mongoose.connect('mongodb+srv://somood:somood@cluster0.os22fcn.mongodb.net/test');
         console.log('Connection has been established successfully.')
     }catch(error){
         console.log(`Unable to connect to the database: ${error}`);
